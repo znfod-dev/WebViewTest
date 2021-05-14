30af62cb7cc122ec4bd7441d98e8d2ee580962da
znfod-dev <znfod.dev@gmail.com>
2021-05-14 14:01:22 +0900

    First commit

---
+++ WebViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebViewTest
+//
+//  Created by ParkJonghyun on 2021/05/14.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
+
+
